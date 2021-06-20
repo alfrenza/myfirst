6ae705128e30e95b03f2b865c60b798d7d6e6628
alfrenza <alfrenzaburhannudin@gmail.com>
2021-06-20 19:20:14 +0700

    Update Terbaru

---
+++ index.js
@@ -0,0 +1,23 @@
+// 5 Primitive type of Javascript
+
+var a = 1; // number
+var b = "string"; // string
+var c = true; // boolean
+var d = {
+    firstName: "Adam",
+    lastName: "El",
+}; // object
+var e; // undefined
+var f = null; // object
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+d.age = 15;
+d.place = "Jakarta";
+d.isLogin = false;
+console.log(d);
+// console.log(typeof c);
+// console.log(typeof d);
+// console.log(typeof e);
+// console.log(typeof f);
